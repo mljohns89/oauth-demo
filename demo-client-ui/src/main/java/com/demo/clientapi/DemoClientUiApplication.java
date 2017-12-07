51458a22f264b2d6115b542ab7626c9b4136008c
michaeljohnson <mjohns39@mix.wvu.edu>
2017-12-06 23:32:19 -0500

    added demo client ui template

---
+++ demo-client-ui/src/main/java/com/demo/clientapi/DemoClientUiApplication.java
@@ -0,0 +1,12 @@
+package com.demo.clientapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoClientUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoClientUiApplication.class, args);
+	}
+}
